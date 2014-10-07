011407be4f82a29f30a9fe6962163a7fb6c2094a
Alex Ward <alex.ward@bbq-media.com>
2014-10-07 18:34:21 +0100

    started building templates and setting up assets

---
+++ www/src/BBQ/Charcoal/WebsiteBundle/Resources/static/scripts/modules/global/init.js
@@ -0,0 +1,4 @@
+$(document).ready( function() {
+    Core.init();
+    Forms.init();
+});
---
+++ www/src/BBQ/Charcoal/WebsiteBundle/Resources/static/scripts/modules/global/thirdparty.js
@@ -0,0 +1,16 @@
+// 3rd party functions - ignored by JShint, use with caution and always cite sources
+/* jshint ignore:start */
+
+// Read a page's GET URL variables and return them as an associative array. (thanks to http://jquery-howto.blogspot.co.uk/2009/09/get-url-parameters-values-with-jquery.html)
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
+/* jshint ignore:end */
